97fea3e3d4d4349fad0c7695dfdfef99456895da
AlvaroJ4 <alvaro.pereiraJ4@gmail.com>
2022-06-20 15:44:33 -0300

    Funçoes do botão

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import sytle from './Botao.module.scss'
 
-class Botao extends React.Component<{ children: string }>{
+class Botao extends React.Component<{ type?: "button" | "submit" | "reset" | undefined , children: string }>{
     render() {
+        const { type = 'button' } = this.props
         return(
-            <button className={sytle.botao}>
+            <button type={type} className={sytle.botao}>
                 {this.props.children}
             </button>
         )
